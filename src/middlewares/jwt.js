b1e6ba20143a0383336b3d916fdfc538ae5e9708
AlvinKP <alvinkusuma98@gmail.com>
2021-11-12 21:31:35 +0700

    tambah checkJWT

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -1,3 +1,18 @@
 const jwt = require("jsonwebtoken");
 
-// const createJWT =
+const checkJWT = (req, res, next) => {
+  const header = req.get("Authorization");
+  if (!token) {
+    return res.status(401).json({ msg: "Unauthorized" });
+  } else {
+    jwt.verify(token, "secret-key", (err, decode) => {
+      if (err) return res.status(401).json({ msg: err.message });
+      else {
+        req.auth = decode.user;
+        next();
+      }
+    });
+  }
+};
+
+module.exports = { checkJWT };
